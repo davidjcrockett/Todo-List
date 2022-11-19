c348a014b98224075b3411255647ea1f87a20c28
David J Crockett <crockett.davidj@gmail.com>
2022-11-18 16:46:41 -0800

    added line of code to link to future CSS + added post method

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
 const app = express();
 
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({ extended : true }));
+
 app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
 
 PORT = 3000;
 
