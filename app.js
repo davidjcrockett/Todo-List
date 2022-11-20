dfcd49e76394d8fbef1ce7efe42a00bdb47ab336
David J Crockett <crockett.davidj@gmail.com>
2022-11-19 19:55:06 -0800

    Added CSS

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended : true }));
 
 app.set("view engine", "ejs");
 
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser : true }, () => {
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser : true , useUnifiedTopology: true,}, () => {
     console.log("Connected to DB!");
 
     app.listen(PORT, () => console.log(`Listening on channel ${PORT}`));
@@ -62,7 +62,7 @@ app
 //DELETE
 app.route("/remove/:id").get((req, res) => {
     const id = req.params.id;
-    TodoTask.findByIdAndRemove(id, err => {
+    TodoSchema.findByIdAndRemove(id, err => {
         if (err) return res.send(500, err);
         res.redirect("/");
         });
