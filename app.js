55a4460417c40566beb7724d12849b781ae8961b
David J Crockett <crockett.davidj@gmail.com>
2022-11-19 16:31:58 -0800

    Added DELETE functionality with findByIdAndRemove

--- app.js
+++ app.js
@@ -52,9 +52,18 @@ app
     })
     .post((req, res) => {
         const id = req.params.id;
-        
+
         TodoSchema.findByIdAndUpdate(id, { content: req.body.content }, err => {
             if (err) return res.send(500, err);
             res.redirect("/");
+        });
+    });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
         });
     });
